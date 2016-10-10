f49ceca4e2696509c48696f3c91153e8b51f2d60
chloroplast <41893204@qq.com>
2016-10-10 22:00:44 +0800

    修改特色分类

---
+++ Application/featuredCategoriesConfig.php
@@ -0,0 +1,43 @@
+<?php
+return [
+    [
+        'title'=>'Escalator Step',
+        'href'=>'http://www.passionelevator.com/Product?filter[status]=0&filter[category]=9',
+        'img'=>'/Global/Style/Home/images/escalator_step.jpg'
+    ],
+    [
+        'title'=>'Escalator Chain',
+        'href'=>'http://www.passionelevator.com/Product?filter[status]=0&filter[category]=10',
+        'img'=>'/Global/Style/Home/images/escalator_chain.jpg'
+    ],
+    [
+        'title'=>'Escalator Handrail',
+        'href'=>'http://www.passionelevator.com/Product?filter[status]=0&filter[category]=8',
+        'img'=>'/Global/Style/Home/images/escalator_handrail.jpg'
+    ],
+    [
+        'title'=>'Escalator Comb Plate',
+        'href'=>'http://www.passionelevator.com/Product?filter[status]=0&filter[category]=11',
+        'img'=>'/Global/Style/Home/images/Escalator_Comb_Plate.jpg'
+    ],
+    [
+        'title'=>'Elevator PCB',
+        'href'=>'http://www.passionelevator.com/Product?filter[status]=0&filter[parentCategory]=15',
+        'img'=>'/Global/Style/Home/images/elevator_PCB.jpg'
+    ],
+    [
+        'title'=>'Elevator Wheel',
+        'href'=>'http://www.passionelevator.com/Product?filter[status]=0&filter[parentCategory]=22',
+        'img'=>'/Global/Style/Home/images/elevator_wheel.jpg'
+    ],
+    [
+        'title'=>'Elevator Inverter',
+        'href'=>'http://www.passionelevator.com/Product?filter[status]=0&filter[category]=16',
+        'img'=>'/Global/Style/Home/images/elevator_inverter.jpg'
+    ],
+    [
+        'title'=>'Elevator Test Tool',
+        'href'=>'http://www.passionelevator.com/Product?filter[status]=0&filter[category]=17',
+        'img'=>'/Global/Style/Home/images/elevator_test_tool.jpg'
+    ]
+];
